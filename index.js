b4940d6032516c325fc652b3a90ae1065614ad85
MoatazNasr <75985257+MoatazNasr@users.noreply.github.com>
2023-12-31 00:35:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const wishlistRoutes = require("./routes/wishlist.js");
 const orderRoutes = require("./routes/order.js");
 const stripeRoutes = require("./routes/stripe.js");
 const cors = require("cors");
+const port = process.env.PORT || 3000;
 
 dotenv.config();
 const app = express();
@@ -42,7 +43,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).json(message);
 });
 
-app.listen(2002, () => {
+app.listen(port, () => {
   console.log("listens at port 2002");
 });
 /try it / 
